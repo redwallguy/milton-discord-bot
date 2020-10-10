f99b4cf4cba45129c74f5adeef6ff1f639858bf9
Dev Vibhu Someshwar <redwallguy@gmail.com>
2020-10-09 22:29:41 -0400

    Added sb, ls, lb commands, added intro placeholder

--- cogs/voice.py
+++ cogs/voice.py
@@ -61,9 +61,20 @@ class VoiceCog(commands.Cog):
         board (str): Name of board to list clips from
 
         Returns:
-        List[str]: Clips of board
+        List[clips]: Clips of board
         """
         return await requests.list_board(board)
+
+    async def get_boards(self):
+        """
+        Gets all boards.
+
+        Parameters: None
+
+        Returns:
+        List[boards]: Boards
+        """
+        return await utils.board_get({})
 
 
     @commands.Cog.listener()
@@ -73,16 +84,60 @@ class VoiceCog(commands.Cog):
         """
         await utils.generate_token()
 
-    @commands.command(aliases=["ls"])
-    async def list_board(self, ctx, board=None):
+    @commands.command(aliases=['sb'])
+    async def switch_board(self, ctx, board):
+        board_list = await self.get_boards()
+        temp_list = [b.get('name') for b in board_list]
+        if board in temp_list:
+            self.board = board
+            await ctx.send("Board switched to " + board + ".")
+        else:
+            await ctx.send("Board does not exist.")
+
+    @commands.command()
+    async def ls(self, ctx, board=None):
         """
+        Lists clips of <board> and their aliases.
+
+        If no board is provided, the current board is used.
+
+        Clips are listed in the form:
+        'clip': ['alias1', 'alias2', ...]
         """
-        # TODO implement
+        if board is None:
+            if self.board is None:
+                await ctx.send("Please switch to or select a board.")
+            else:
+                board = self.board
         board_list = await self.get_clips(board)
         formatted_response = "Board: " + board + "\n----------\n"
         for clip in board_list:
             formatted_response += "'" + clip.get('name') + "': " + str(clip.get('aliases')) + "\n"
         await ctx.send(formatted_response)
+
+    @commands.command(aliases=['lb'])
+    async def boards(self, ctx):
+        """
+        Lists all boards.
+        """
+        board_list = await self.get_boards()
+        formatted_response = "Boards\n----------\n"
+        for board in board_list:
+            formatted_response += board.get('name') + "\n"
+        await ctx.send(formatted_response)
+
+    @commands.group()
+    async def intro(self, ctx):
+        if ctx.invoked_subcommand is None:
+            pass # TODO show user's intro
+    
+    @intro.command(name='set')
+    async def set_intro(self, ctx, clip, board):
+        pass
+
+    @intro.command(name='delete')
+    async def delete_intro(self, ctx):
+        pass
 
     @commands.command()
     async def play(self, ctx, clip: str, board: str):
@@ -102,11 +157,3 @@ class VoiceCog(commands.Cog):
         Disconnects bot from voice.
         """
         await self.disconnect_voice()
-
-    @play.error
-    async def play_error(self, ctx, error):
-        logger.info(error)
-
-    @list_board.error
-    async def list_board_error(self, ctx, error):
-        logger.info(error)
